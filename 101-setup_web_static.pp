adc2e4ea30e33f2e033914fb133cf780daea103d
calebkech <calebkemboi61@gmail.com>
2024-04-04 09:02:13 -0700

    update puppet file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,4 @@
-# Configures a web server for deployment of web_static.
-
-# Nginx configuration file
+# Define the Nginx configuration
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -24,11 +22,13 @@ $nginx_conf = "server {
     }
 }"
 
+# Install Nginx package
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
 } ->
 
+# Create necessary directories
 file { '/data':
   ensure  => 'directory'
 } ->
@@ -49,20 +49,24 @@ file { '/data/web_static/shared':
   ensure => 'directory'
 } ->
 
+# Create a sample index.html file in /data/web_static/releases/test
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
+  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n"
 } ->
 
+# Create a symbolic link to point to the current release
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
 } ->
 
+# Ensure ownership is set to ubuntu:ubuntu
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
+# Ensure /var/www/html exists
 file { '/var/www':
   ensure => 'directory'
 } ->
@@ -71,21 +75,25 @@ file { '/var/www/html':
   ensure => 'directory'
 } ->
 
+# Create a default index.html in /var/www/html
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
+  content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School Nginx\n  </body>\n</html>\n"
 } ->
 
+# Create a 404.html in /var/www/html
 file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
+  content => "<html>\n  <head>\n  </head>\n  <body>\n    Ceci n'est pas une page\n  </body>\n</html>\n"
 } ->
 
+# Apply the Nginx configuration
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
 } ->
 
+# Restart Nginx service
 exec { 'nginx restart':
   path => '/etc/init.d/'
 }
